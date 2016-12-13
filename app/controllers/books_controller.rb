1897957ff8008bedf68ca9c26edc7cc1f639ced5
Zayne Abraham <Bram@Zaynes-MacBook-Pro.local>
2016-12-13 11:29:45 -0700

    updated attributes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+	
+	def index
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save 
+			redirect_to root_path
+		else 
+			render 'new'
+		end
+	end
+
+	private 
+		
+		def book_params
+			params.require(:book).permit(:title, :description, :author)
+		end
+
+end
